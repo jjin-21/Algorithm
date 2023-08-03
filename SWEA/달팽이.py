53e9370337d7cd70c5738338c9529415aa01c91a
LeeJaeJin <113574354+jjin-21@users.noreply.github.com>
2023-08-03 21:18:11 +0900

    SWEA

--- SWEA/달팽이.py
+++ SWEA/달팽이.py
@@ -1,26 +1,22 @@
 def snail(N):
     ls = [[0 for _ in range(N)] for _ in range(N)]
     num = 1
-    direction = 0
-    di = [0, 1, 0, -1]
-    dj = [1, 0, -1, 0]
-    i = 0
-    j = 0
+    dir = 0
+    i,j = 0,0
+    di = [0,1,0,-1]
+    dj = [1,0,-1,0]
     while num <= N**2:
         ls[i][j] = num
         num += 1
-
-        ni = i + di[direction]
-        nj = j + dj[direction]
-        if 0 <= ni < N and 0 <= nj < N and ls[ni][nj] == 0:
-            i = ni
-            j = nj
+        ni = i+di[dir]
+        nj = j+dj[dir]
+        if 0 <= ni < N and 0 <= nj < N and ls[ni][nj]==0:
+            i,j = ni,nj
         else:
-            direction = (direction + 1) % 4
-            i = i + di[direction]
-            j = j + dj[direction]
+            dir = (dir+1)%4
+            i = i+di[dir]
+            j = j+dj[dir]
     return ls
-
 T = int(input())
 for t in range(1,T+1):
     N = int(input())
